c4ab3bc27ffe6a45950101cf4331160fbff79e35
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2008-06-25 14:43:38 +0200

    Plot thermodynamic equilibrium properties.

---
+++ src/mdplot/en.py
@@ -0,0 +1,110 @@
+#
+# mdplot - Molecular Dynamics simulation plotter
+#
+# Copyright (C) 2008  Peter Colberg <peter.colberg@physik.uni-muenchen.de>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+
+import os, os.path
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+import numpy
+import sys
+import tables
+
+
+"""
+Plot mean total energy per particle
+"""
+def plot(args):
+    try:
+        f = tables.openFile(args.input, mode='r')
+    except IOError:
+        raise SystemExit('failed to open HDF5 file: %s' % args.input)
+
+    H5 = f.root
+    try:
+        data = H5._v_children[args.type]
+        x = data[:, 0]
+        if args.type == 'VCM':
+            # positional coordinates dimension
+            dim = H5.parameters.mdsim._v_attrs.dimension
+            # calculate center of velocity magnitude
+            if dim == 3:
+                y = numpy.sqrt(data[:, 1] * data[:, 1] + data[:, 2] * data[:, 2] + data[:, 3] * data[:, 3])
+            else:
+                y = numpy.sqrt(data[:, 1] * data[:, 1] + data[:, 2] * data[:, 2])
+        else:
+            y = data[:, 1]
+
+    except tables.exceptions.NoSuchNodeError:
+        raise SystemExit('missing simulation data in file: %s' % args.input)
+
+    finally:
+        f.close()
+
+    if args.xaxis:
+        # limit data points to given x-axis range
+        x0 = max(x.min(), args.xaxis[0])
+        x1 = min(x.max(), args.xaxis[1])
+        i = numpy.where((x >= x0) & (x <= x1))
+        x = x[i]
+        y = y[i]
+    else:
+        x0 = x.min()
+        x1 = x.max()
+
+    ylabel = {
+        'ETOT':  r'$\langle E\rangle / \epsilon$',
+        'EPOT':  r'$\langle U\rangle / \epsilon$',
+        'EKIN':  r'$\langle T\rangle / \epsilon$',
+        'PRESS': r'$\langle P\rangle$',
+        'TEMP':  r'$\langle T\rangle$',
+        'VCM':   r'$\vert\langle \textbf{v}^*\rangle\vert$',
+    }
+    mlabel = {
+        'ETOT':  r'$\langle\langle E\rangle\rangle_{t^*}$',
+        'EPOT':  r'$\langle\langle U\rangle\rangle_{t^*}$',
+        'EKIN':  r'$\langle\langle T\rangle\rangle_{t^*}$',
+        'PRESS': r'$\langle\langle P\rangle\rangle_{t^*}$',
+        'TEMP':  r'$\langle\langle T\rangle\rangle_{t^*}$',
+        'VCM':   r'$\langle\vert\langle \textbf{v}^*\rangle\vert\rangle_{t^*}$',
+    }
+
+    plt.figure(1, [11.7, 8.3])
+    ax = plt.axes()
+    m, s = numpy.mean(y), numpy.std(y)
+    ax.fill([x0, x0, x1, x1], [m - s, m + s, m + s, m - s], 'm', alpha=0.1)
+    ax.plot(x, y, color='m')
+    ax.plot([x0, x1], [m] * 2, '--', color='m', alpha=0.5)
+    ax.text(x1 + (x1 - x0) / 100, m, mlabel[args.type], verticalalignment='center', horizontalalignment='left')
+    major_formatter = ticker.FormatStrFormatter('%g')
+    ax.xaxis.set_major_formatter(major_formatter)
+    ax.yaxis.set_major_formatter(major_formatter)
+    plt.xlabel(r'$t^*$')
+    plt.ylabel(ylabel[args.type])
+    plt.savefig(args.output)
+
+
+def add_parser(subparsers):
+    parser = subparsers.add_parser(command, help='thermal equilibrium properties')
+    parser.add_argument('input', metavar='INPUT', help='HDF5 energy file')
+    parser.add_argument('--type', required=True, choices=['ETOT', 'EPOT', 'EKIN', 'PRESS', 'TEMP', 'VCM'], help='thermal equilibrium property')
+    parser.add_argument('--xaxis', metavar='VALUE', type=float, nargs=2, help='limit x-axis to given range')
+    parser.add_argument('--output', required=True, help='output filename')
+
+command = 'en'
+
