2ebafaea153a4b15de0045d2eaf783951ad73f1c
Peter Colberg <peter@colberg.org>
2008-11-30 15:37:20 +0100

    pdf: plot multiple input files.

--- src/mdplot/pdf.py
+++ src/mdplot/pdf.py
@@ -19,8 +19,9 @@
 #
 
 import os, os.path
-import numpy
+from numpy import *
 from scipy.special import gamma
+import mdplot.label
 import sys
 import tables
 
@@ -33,80 +34,98 @@ Computer Simulation of Liquids, 1989,
 Oxford University Press, pp. 55, 183-184
 """
 def plot(args):
-    from matplotlib import pyplot as plt
+    from matplotlib import pyplot as plot
 
-    try:
-        f = tables.openFile(args.input, mode='r')
-    except IOError:
-        raise SystemExit('failed to open HDF5 file: %s' % args.input)
+    ax = plot.axes()
+    ax.axhline(y=1, color='black', lw=0.5)
+    ax.set_color_cycle(args.colors)
 
-    # HDF5 root group
-    H5 = f.root
-
-    try:
+    for i, fn in enumerate(args.input):
         try:
-            # particle positions of phase space sample
-            r = H5.trajectory.r[args.sample]
-            # simulation time
-            time = H5.trajectory.t[args.sample]
-        except IndexError:
-            raise SystemExit('out-of-bounds phase space sample number')
+            f = tables.openFile(fn, mode='r')
+        except IOError:
+            raise SystemExit('failed to open HDF5 file: %s' % fn)
 
-        # periodic simulation box length
-        box = H5.param.mdsim._v_attrs.box_length
-        # number of particles
-        N = H5.param.mdsim._v_attrs.particles
-        # positional coordinates dimension
-        dim = H5.param.mdsim._v_attrs.dimension
-        # particle density
-        density = H5.param.mdsim._v_attrs.density
+        # HDF5 root group
+        H5 = f.root
 
-        cutoff = args.cutoff or box / 2
-        H = numpy.zeros(args.bins)
-        for (i, j) in enumerate(range(N - 1, 0, -1)):
-            # particle distance vectors
-            dr = r[:j] - r[i + 1:]
-            # minimum image distances
-            dr = dr - numpy.round(dr / box) * box
-            # squared minimum image distances
-            rr = dr[:, 0] * dr[:, 0] + dr[:, 1] * dr[:, 1]
-            if dim == 3:
-                rr = rr + dr[:, 2] * dr[:, 2]
-            # accumulate histogram of minimum image distances
-            h, bins = numpy.histogram(numpy.sqrt(rr), bins=args.bins, range=(0, cutoff), new=True)
-            H = H + 2 * h
+        try:
+            try:
+                # particle positions of phase space sample
+                r = H5.trajectory.r[args.sample]
+                # simulation time
+                time = H5.trajectory.t[args.sample]
+            except IndexError:
+                raise SystemExit('out-of-bounds phase space sample number')
 
-    except tables.exceptions.NoSuchNodeError:
-        raise SystemExit('missing simulation data in file: %s' % args.input)
+            # periodic simulation box length
+            box = H5.param.mdsim._v_attrs.box_length
+            # number of particles
+            N = H5.param.mdsim._v_attrs.particles
+            # positional coordinates dimension
+            dim = H5.param.mdsim._v_attrs.dimension
+            # particle density
+            density = H5.param.mdsim._v_attrs.density
 
-    finally:
-        f.close()
+            cutoff = args.xlim or (0, box / 2)
+            H = zeros(args.bins)
+            for (i, j) in enumerate(range(N - 1, 0, -1)):
+                # particle distance vectors
+                dr = r[:j] - r[i + 1:]
+                # minimum image distances
+                dr = dr - round_(dr / box) * box
+                # squared minimum image distances
+                rr = dr[:, 0] * dr[:, 0] + dr[:, 1] * dr[:, 1]
+                if dim == 3:
+                    rr = rr + dr[:, 2] * dr[:, 2]
+                # accumulate histogram of minimum image distances
+                h, bins = histogram(sqrt(rr), bins=args.bins, range=cutoff, new=True)
+                H = H + 2 * h
 
-    # volume of n-dimensional unit sphere
-    Vn = numpy.power(numpy.pi, dim / 2.) / gamma(dim / 2. + 1.)
-    # average number of atoms in ideal gas per interval
-    n = Vn * density * (numpy.power(bins[1:], dim) - numpy.power(bins[:-1], dim))
-    # compute pair correlation function
-    g = H / n / N
+            if args.label:
+                label = args.label[i % len(args.label)] % mdplot.label.attributes(H5.param)
+            else:
+                basen = os.path.splitext(os.path.basename(fn))[0]
+                label = basen.replace('_', r'\_')
 
-    plt.plot(bins[:-1], g, color='m')
-    plt.axis([0, cutoff, 0, max(g)])
-    plt.xlabel(args.xlabel or r'particle distance $|\mathbf{r}_{ij}| / \sigma$')
-    plt.ylabel(args.ylabel or r'pair distribution function $g(|\mathbf{r}_{ij}| / \sigma)$')
+        except tables.exceptions.NoSuchNodeError:
+            raise SystemExit('missing simulation data in file: %s' % fn)
+
+        finally:
+            f.close()
+
+        # volume of n-dimensional unit sphere
+        Vn = power(pi, dim / 2.) / gamma(dim / 2. + 1.)
+        # average number of atoms in ideal gas per interval
+        n = Vn * density * (power(bins[1:], dim) - power(bins[:-1], dim))
+        # compute pair correlation function
+        g = H / n / N
+
+        ax.plot(bins[:-1], g, marker='.', label=label)
+
+    ax.axis('tight')
+    if args.xlim:
+        plot.setp(ax, xlim=args.xlim)
+    if args.ylim:
+        plot.setp(ax, ylim=args.ylim)
+
+    plot.setp(ax, xlabel=args.xlabel or r'$|\mathbf{r}_{ij}| / \sigma$')
+    plot.setp(ax, ylabel=args.ylabel or r'$g(|\mathbf{r}_{ij}| / \sigma)$')
     l = ax.legend(loc=args.legend)
     l.legendPatch.set_alpha(0.7)
 
     if args.output is None:
-        plt.show()
+        plot.show()
     else:
-        plt.savefig(args.output, dpi=args.dpi)
+        plot.savefig(args.output, dpi=args.dpi)
 
 
 def add_parser(subparsers):
     parser = subparsers.add_parser('pdf', help='pair distribution function')
-    parser.add_argument('input', metavar='INPUT', help='HDF5 trajectory file')
+    parser.add_argument('input', metavar='INPUT', nargs='+', help='HDF5 trajectory file')
     parser.add_argument('--sample', type=int, help='phase space sample number')
     parser.add_argument('--bins', type=int, help='number of histogram bins')
-    parser.add_argument('--cutoff', type=float, help='truncate function at given distance')
-    parser.set_defaults(sample=-1, bins=1000)
+    parser.add_argument('--xlim', metavar='VALUE', type=float, nargs=2, help='limit x-axis to given range')
+    parser.add_argument('--ylim', metavar='VALUE', type=float, nargs=2, help='limit y-axis to given range')
+    parser.set_defaults(sample=-1, bins=100)
 
