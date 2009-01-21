d154c0052947f9901ef16e553934cbb26ff2fdd9
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2009-01-21 12:07:44 +0100

    mdplot: add python setup script.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from subprocess import Popen, PIPE
+
+def parse_git_version():
+    version = 'unkown'
+    try:
+        version = Popen(['git', 'describe', '--always'], stdout=PIPE).communicate()[0] or version
+    except OSError:
+        pass
+    return version
+
+setup(name = 'mdplot',
+      version = parse_git_version(),
+      description = 'Molecular Dynamics simulation plotter',
+      author = 'Peter Colberg',
+      author_email = 'peter.colberg@physik.uni-muenchen.de',
+      packages = ['mdplot'],
+      scripts = ['bin/mdplot'],
+      license = 'GPL',
+      )
+
