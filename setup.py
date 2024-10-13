6dc467f239f94096c1708f300780b6d0eae870d5
Felix Höfling <f.hoefling@fu-berlin.de>
2024-10-13 20:54:31 +0200

    setup: switch to setuptools for Python ≥ 3.12
    When upgrading from Python 3.10 to 3.12 and NumPy to 1.26 (Ubuntu
    24.04), numpy.distutils has disappeard. So we need to switch to
    setuptools.
    
    As a consequence, Git descriptions of dirty (non-tagged) commits are
    disallowed in Python, so we have to fix the version string.
    
    Also, NumPy's include path must be specified by hand.

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from numpy.distutils.core import setup, Extension
+from setuptools import setup, Extension
 from subprocess import check_output
+import numpy
 
 def parse_git_version():
     version = 'unkown'
     try:
         version = check_output(['git', 'describe', '--always']).decode('utf-8').strip() or version
+        # make version string compliant with Python restrictions for non-release Git commits
+        version = version.replace('-', '+dirty', 1)     # replace first '-' by '+dirty'
+        version = version.replace('-', '.')             # replace remaining '-' by '.'
     except OSError:
         pass
     return version
 
 module1 = Extension('h5mdtools._plot.ext',
                      sources = ['h5mdtools/_plot/c/ext.cpp', 'h5mdtools/_plot/c/ssf.cpp'],
+                     include_dirs = [numpy.get_include()],
                      extra_compile_args = ['-O3', '-fopenmp', '-mtune=native', '-Wall'],
                      extra_link_args = ['-fopenmp'])
 
@@ -23,7 +28,7 @@ setup(name = 'h5md-tools',
       author = ('Felix Höfling'),
       author_email = ('f.hoefling@fu-berlin.de'),
       packages = ['h5mdtools', 'h5mdtools._plot'],
-      scripts = ['bin/h5md'],
+      console_scripts = ['bin/h5md'],
       package_data={'h5mdtools._plot': ['gpu/ssf_kernel.cu']},
       ext_modules = [module1,],
       license = 'GPL'
